c62880bbc421e37ec28168c7148f0a391b60a2b0
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2019-08-20 20:35:13 -0300

    Adicionando Cards

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,12 +3,34 @@ import React from 'react';
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { 
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 150000.000,50</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 500,00 recebida de Luis Gustavo hoje às 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   )
